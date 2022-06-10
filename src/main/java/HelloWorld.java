b6895e4deb4096d6dda2c2defe762a855a85d7bf
ron <ron.lobo@yahoo.com>
2022-06-10 20:02:28 +0100

    0004: updated HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        int i = 55;
         System.out.println("Hello C242");
+        i++;
+        System.out.println("Good Day C242");
+        i += 5;
+        System.out.println("Bye C242");
     }
 }
