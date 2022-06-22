952aab8cfdb95d237123129650015f1d219aa96a
ron <ron.lobo@yahoo.com>
2022-06-22 09:39:17 +0100

    0016: sets

--- src/main/java/org/ron/intro/CollectionsIntro.java
+++ src/main/java/org/ron/intro/CollectionsIntro.java
@@ -2,21 +2,54 @@ package org.ron.intro;
 
 import org.apache.commons.collections4.list.TreeList;
 import org.ron.vehicle.Car;
+import org.ron.vehicle.Engine;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 
 public class CollectionsIntro {
 
     private Random random = new Random();
+    private Engine e1 = new Engine("V", 4, 2000);
+    private Engine e2 = new Engine("V", 6, 2400);
+    private Engine e3 = new Engine("V", 8, 3000);
+    private Engine e4 = new Engine("S", 4, 1200);
+    private Engine e5 = new Engine("S", 6, 1600);
 
     public static void main(String[] args) {
         CollectionsIntro ci = new CollectionsIntro();
 //        ci.arrays();
         ci.lists();
+//        ci.sets();
+        ci.maps();
+    }
+
+    private void maps() {
+    }
+
+    private void sets() {
+        Set<String> drivers1 = new HashSet<>();
+        Set<String> drivers2 = new TreeSet<>();
+
+        drivers1.add("V");
+
+        setHelper("HashSet", drivers1);
+        setHelper("TreeSet", drivers2);
+    }
+
+    private void setHelper(String name, Set<String> drivers) {
+        System.out.println("set: " + name + ", size = " + drivers.size());
+        drivers.add("Jungkook");
+        drivers.add("V");
+        drivers.add("Suga");
+        drivers.add("RM");
+        drivers.add("Jungkook");
+        drivers.add("Jimin");
+        drivers.add("Jungkook");
+        for (String driver : drivers) {
+            System.out.println("  " + driver);
+        }
+        System.out.println(" new size = " + drivers.size());
     }
 
     private void lists() {
@@ -28,10 +61,22 @@ public class CollectionsIntro {
         listHelper("ArrayList", intList1);
         listHelper("LinkedList", intList2);
         listHelper("TreeList", intList3);
+
+        removeDuplicates("ArrayList", intList1);
+        removeDuplicates("LinkedList", intList2);
+        removeDuplicates("TreeList", intList3);
+    }
+
+    private void removeDuplicates(String name, List<Integer> intList) {
+        Set<Integer> intSet = new HashSet<>(intList);
+        System.out.printf("list %s contains %d items, #unique=%d %n", name, intList.size(), intSet.size());
+
+        intList.clear();
+        intList.addAll(intSet);
     }
 
     private void listHelper(String text, List<Integer> list) {
-        while (random.nextDouble() > 0.15) {
+        while (random.nextDouble() > 0.1) {
             list.add(random.nextInt(10));
         }
         list.add(0, 555);
